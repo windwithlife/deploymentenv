e35262ae378651c1e5386afacb4d99165880001e
windwithlife@hotmail.com <windwithlife@hotmail.com>
2020-11-05 16:31:52 +0800

    add init k8s with cert-manager, nginx-ingress support

---
+++ script/start.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+kubectl apply --validate=false -f https://github.com/jetstack/cert-manager/releases/download/v1.0.3/cert-manager.yaml
+kubectl apply -f ../k8s/sources/gateway/nginx-ingress-prod.yaml
